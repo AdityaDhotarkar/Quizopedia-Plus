9b9535f03c6f849e75d1cb2561f4919a214e4334
AdityaDhotarkar <aditya.dhotarkar19@vit.edu>
2021-11-18 12:01:37 +0530

    Adding Quiz Module

---
+++ js/questions.js
@@ -0,0 +1,7 @@
+var questions = [
+  ["What does CSS stand for?", " Colorful Style Sheets", " Cascading Style Sheets", " Creative Style Sheets"," Computer Style Sheets", "2"],
+  ["Which HTML attribute is used to define inline styles?", "styles", "class", " style", "font","4"],
+  ["Which CSS property controls the text size?", "font-style", "text-style", "font-size", "texxt-size","3"],
+  ["What does HTML stand for?", "  Hyper Text Markup Language", "  Home Tool Markup Language", " Hyperlinks and Text Markup Language", "None of these","1"],
+  ["How do you select an element with id 'demo'?", "  .demo", " #demo", " *demo", "demo","2"],
+  ];
